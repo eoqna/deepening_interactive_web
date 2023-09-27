230b53d34b688041bc241d77cbce3a8a98e6346e
eoqna <eoqna@naver.com>
2023-09-27 13:03:32 +0900

    코드 수정

--- section6/js/motion_m.js
+++ section6/js/motion_m.js
@@ -6,8 +6,8 @@ let my = 0;
 let isMobile = false;
 
 const loopMobile = () => {
-  mx += (x - mx) * .1;
-  my += (y - my) * .1;
+  mx += (x - mx) * 0.1;
+  my += (y - my) * 0.1;
 
   wrap.style.transform = `translate3d(-50%, -50%, 0) rotateX(${my - 50}deg) rotateY(${mx}deg)`;
 
@@ -15,8 +15,8 @@ const loopMobile = () => {
 };
 
 const loop = () => {
-  mx += (x - mx) * .1;
-  my += (y - my) * .1;
+  mx += (x - mx) * 0.1;
+  my += (y - my) * 0.1;
   //가속도 설정. 뒤의 값을 변경하면 가속도 값 변경
 
   wrap.style.transform = `translate3d(-50%, -50%, 0) rotateX(${my / 10}deg) rotateY(${-mx / 10}deg)`;
